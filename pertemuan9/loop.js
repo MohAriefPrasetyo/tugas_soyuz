5935d3ee264d4b3ca89212db0a038236614a6d35
ariefprasetyo <alipoprasetyo@gmail.com>
2025-05-11 16:13:31 +0800

    pasti sudah sulit

---
+++ pertemuan9/loop.js
@@ -0,0 +1,102 @@
+/*for(let i = 0; i < 5; i++){
+    console.log("ini adalah iterasi ke- " + (i+1) + ". nilai i adalah" + i);
+}*/
+
+// let i = 0;
+// while(i < 5){
+//     console.log("ini adalah iterasi ke- " + (i+1) + ". nilai i adalah" + i);
+//     i++;
+// }
+// let jawaban = "";
+// while(jawaban !== "apel"){
+//     jawaban = "tebak buah lagi";
+// }
+
+// //break dan continue//
+// for (let i = 0; i < 5; i++) {
+//   console.log("ini adalah iterasi ke- " + (i + 1) + ". nilai i adalah" + i);
+// }
+
+//infinit loop//
+// for (let i = 10; i <= 10; i--) {
+//   console.log(i);
+// }
+
+// nested loop//
+// for (let baris = 1; baris <= 2; baris++) {
+//     for (let kolom = 1; kolom <=3; kolom++){
+//         console.log('ini adalah baris' + baris +'.kolom' + kolom)
+//     }
+// }
+
+//printing patterns//
+// for (let i = 1; i <= 5; i++){
+//     let arif = ''
+//     for (let j = 1; j <= i; j++){
+//         arif += '*'
+//     }
+//     console.log(arif)
+// }
+
+//function//
+// function arifGanteng (){
+//     console.log ("hello world");
+// }
+// arifGanteng()
+
+//parameter//
+// function addTwoNumber (firstNumber, secondNumber){
+//     let result = firstNumber + secondNumber;
+//     console.log(result);
+// }
+// addTwoNumber(2, 3);
+
+//function expression//
+// let sayHello = function (){
+//     console.log("hello world");
+// }
+
+//arrow function//
+// const isBlueColor = color => {
+// if (color === 'blue'){
+//     return ' this color is blue'
+// }else {
+//     return 'oh no this not blue'
+//   }
+// }
+
+// console.log(isBlueColor('red'))
+// console.log(isBlueColor('blue'))
+
+//implisit teada return//
+// const sayHi = name => 'hi' + name
+// console.log(sayHi('rif tolong rif'))
+
+
+// const getProfileObject = (firstname, lastname) => {
+//     return{
+//         fullname : firstname + ' ' + lastname
+//     }
+// }
+// console.log(getProfileObject("arif", "farhan"));
+
+// let myAction = "I am learning Javascript";
+// console.log(myAction.length);
+// console.log(myAction.toLowerCase());
+// console.log(myAction.toUpperCase());
+// console.log(myAction.split(" "));
+// console.log(myAction.substring(0, 4));
+
+
+// let pass = "12345678";
+// console.log(pass)
+// pass = Number(pass)
+// console.log(pass)
+
+
+// console.log(Math.random())
+// console.log(Math.round(0, 5));
+
+console.log(isNaN('314f'))
+console.log(isNaN('3140' ))
+
