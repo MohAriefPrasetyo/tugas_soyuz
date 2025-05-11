ce36dee2ee79fc3cfbf52a6a673df33ea84b50c8
ariefprasetyo <alipoprasetyo@gmail.com>
2025-05-11 16:13:06 +0800

    mulai gacorr

---
+++ pertemuan8/index.js
@@ -0,0 +1,17 @@
+console.log ("halo maipren");
+
+let soyuz = "halo soyuz";
+console.log(typeof soyuz);
+
+let angka = 1234;
+console.log(typeof angka)
+
+if(angka > 1000){
+    console.log("angka lebih dari 1000")
+}
+
+console.log(1 > 2)
+console.log(1 < 2)
+console.log(1 >= 2)
+console.log(1 <= 2)
+
